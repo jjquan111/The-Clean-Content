4132965d940a2c781c3efc27b2e39f874088391f
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-05 12:32:48 -0700

    Delete api directory

--- api/route/href_register.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.views import APIView
-from django.shortcuts import render
-
-
-# 注册
-class Register(APIView):
-    def get(self,request):
-        return render(request, "ui-register.html")
--- api/route/href_teacher.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.views import APIView
-from django.shortcuts import render
-
-
-
-class Subject(APIView):
-    def get(self,request):
-        return render(request, "pro-teacher.html")
