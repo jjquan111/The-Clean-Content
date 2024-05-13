dbf4ef206c2bd73498a7c22d1fe882101518939b
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-13 16:46:20 -0700

    Add files via upload

---
+++ dal/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from dal import models
+
+
+admin.site.site_header = 'The Clean Content'
+admin.site.site_title = 'Login The Clean Content'
+admin.site.index_title = 'The Clean Content后台管理'
+
+# Register your models here.
+
+
+class DetectsAdmin(admin.ModelAdmin):
+    list_display = ('id','detectcontent','detectresult')
+admin.site.register(models.detects,DetectsAdmin)
+
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'password','email','name','classname','create_time','phone')
+
+admin.site.register(models.UserInfo, UserAdmin)
+
+
+
