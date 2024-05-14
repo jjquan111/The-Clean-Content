24ae1053d25bc1789310909f51a1b2138498483d
Jerry Quan <117068761+jjquan111@users.noreply.github.com>
2024-05-13 18:23:28 -0700

    Update add_subject.py

--- api/add_subject.py
+++ api/add_subject.py
@@ -10,19 +10,19 @@ class AddSubject(APIView):
     def post(self,request):
         username = str(request.data.get("username"))
         subject_title_id = str(request.data.get("subject_title_id"))
-        user_id = models.UserInfo.objects.get(username=username).pk     #  学生ID
+        user_id = models.UserInfo.objects.get(username=username).pk  
 
-        subject_title_obj = models.Subject.objects.get(pk=subject_title_id) # 课程ID
+        subject_title_obj = models.Subject.objects.get(pk=subject_title_id) 
         message = {}
         try:
-            # 课程ID 多对多 添加对应字段的 学生ID
+
             subject_title_obj.subject_student.add(user_id)
 
             message['code'] = 200
-            message['message'] = "添加成功"
+            message['message'] = "success"
             return JsonResponse(message)
         except Exception as e:
             print(e)
             message['code'] = 444
-            message['message'] = "添加失败"
+            message['message'] = "faile"
             return JsonResponse(message)
